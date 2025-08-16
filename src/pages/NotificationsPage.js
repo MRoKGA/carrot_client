e41cb540d040680aab06c0ee4a3c88e174e841ba
euijunlee <euijunlee@naver.com>
2025-08-16 21:39:15 +0900

    ci/cd 마지막 테스트

--- src/pages/NotificationsPage.js
+++ src/pages/NotificationsPage.js
@@ -9,7 +9,7 @@ const chips = ['중고거래', '알바', '부동산', '중고차'];
 
 const activityMock = [
   { id: 1, iconBg: '#FFB84D', iconEmoji: '🏆', title: '[어워즈] 4,000명이 넘는 사장님이 도전중', time: '7시간 전', moreCount: 12 },
-  { id: 2, iconBg: '#FF7A00', iconEmoji: '👥', title: '드레이크님, 회기동 모임 찾으시나요? 이번주의 인기 모임을 준비했어요.', time: '8일 전', moreCount: 2 },
+  { id: 2, iconBg: '#FF7A00', iconEmoji: '👥', title: '드레이크님, 회기동 게이 모임 찾으시나요? 이번주의 인기 모임을 준비했어요.', time: '8일 전', moreCount: 2 },
   { id: 3, iconBg: '#FF4D4D', iconEmoji: '❤️', title: '다른 이웃이 "샤오미 미에어2" 글에 관심을 눌렀어요. 고민하는 사이 곧 팔릴지도 몰라요🥺', time: '11일 전', moreCount: 2, thumb: 'https://via.placeholder.com/60x60.png?text=IMG' },
 ];
 
